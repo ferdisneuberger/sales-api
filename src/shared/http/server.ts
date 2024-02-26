4a30d4cf3701565b1c5411e359cf4b0865ed24a5
Fernando Neuberger <fernandoftneuberger@gmail.com>
2024-02-26 08:30:45 -0300

    feat: Adicionar middleware para interceptar os erros da aplicação e customizar as respostas

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -8,6 +9,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        meessage: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server started on port ${process.env.SERVER_PORT}`);
